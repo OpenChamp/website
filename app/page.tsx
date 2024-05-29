d747e97ae72de1a95379a56b87803b5482cc96f4
majicmaj <majd.tarbin@outlook.com>
2024-05-29 15:35:12 -0400

    refactor: Update navigation styling and layout in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       <div className="grid grid-cols-1 grid-rows-2 min-h-screen">
         <div className="grid grid-cols-1 grid-rows-1">
           {/* Nav */}
-          <div className="items-center grid grid-cols-3 gap-8 p-4 px-8 absolute top-0 z-10 w-full bg-gradient-to-b from-stone-950 to-transparent">
+          <div className="flex justify-center items-center xl:grid xl:grid-cols-3 gap-4 xl:gap-8 p-2 px-4 xl:p-4 xl:px-8 absolute top-0 z-10 w-full bg-gradient-to-b from-stone-950 to-transparent">
             <img
               src="/champeye.webp"
               alt="OpenChamp Logo"
@@ -13,7 +13,7 @@ export default function Home() {
               width={48}
               className="select-text"
             />
-            <nav className="flex justify-center overflow-auto">
+            <nav className="justify-center overflow-auto hidden xl:flex">
               <ul className="flex gap-12 font-bold text-sm tracking-wider text-stone-200">
                 <li>
                   <a href="/">GAME*</a>
@@ -26,14 +26,14 @@ export default function Home() {
                 </li>
               </ul>
             </nav>
-            <div className="flex justify-end">
+
+            <div className="justify-end hidden xl:flex">
               <a
                 href="https://discord.gg/f6DGjvTWYT"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 <button className="px-4 py-2 bg-cyan-800 font-bold hover:bg-cyan-400 transition-all duration-300 hover:text-stone-800 rounded-lg">
-                  {/* DOWNLOAD */}
                   DISCORD
                 </button>
               </a>
@@ -86,7 +86,7 @@ export default function Home() {
               </p>
               <div className="flex gap-8">
                 <button className="px-6 py-4 text-stone-100 bg-[#b68236] font-bold border border-[#b99f4c] hover:bg-[#f3da8b] hover:text-stone-800 transition-all duration-300">
-                  BROWSE CHAMPIONS*
+                  CHAMPIONS*
                 </button>
                 <button className="px-6 py-4 text-stone-100 bg-cyan-800 font-bold hover:bg-cyan-400 hover:text-stone-800 transition-all duration-300">
                   PLAY*
